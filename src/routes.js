63106023646f52ceacca02e390482c760de6bd6e
Pablo Vicente <pablo.vicente@outlook.com.br>
2021-10-10 19:44:57 -0300

    Add Controllers, Calc rules and ejs in HTML

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,169 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
+const Profile = {
+    data: {
+        name: "Pablo Vicente",
+        avatar: "https://github.com/pablo-vicente.png",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 75
+    },
+    controllers: {
+        index(req, res) {
+            return res.render(views + "profile", {
+                profile: Profile.data
+            })
+        },
 
-const profile = {
-    name: "Pablo Vicente",
-    avatar: "https://avatars.githubusercontent.com/u/28940231?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4
+        update(req, res) {
+
+            const data = req.body
+
+            const weeksPerYear = 52
+            const weekPerMoth = (weeksPerYear - data["vacation-per-year"]) / 12
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+            const mothlyTotalHours = weekTotalHours * weekPerMoth
+            const valueHour = data["monthly-budget"] / mothlyTotalHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour
+            }
+
+            return res.redirect("/profile")
+        }
+    }
 }
 
+const Job = {
+    data: [
+        {
+            id: 1,
+            name: "Pizzaria Gulosa",
+            "daily-hours": 2,
+            "total-hours": 1,
+            created_at: Date.now()
+        },
+        {
+            id: 2,
+            name: "OneTwo Project",
+            "daily-hours": 3,
+            "total-hours": 47,
+            created_at: Date.now()
+        }],
+    controllers: {
+        index(req, res) {
+
+            const updatedJobs = Job.data.map((job) => {
+                const remaining = Job.services.remainingDays(job);
+                const status = remaining <= 0 ? 'done' : 'progress';
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
+                };
+            })
+
+            return res.render(views + "index", { jobs: updatedJobs })
+        },
+        create(req, res) {
+            return res.render(views + "job")
+        },
+        save(req, res) {
+            const lastId = Job.data[Job.data.length - 1]?.id || 0;
+
+            Job.data.push({
+                id: lastId + 1,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                created_at: Date.now() // Atribuindo data hoje
+            })
+
+            return res.redirect("/")
+        },
+        show(req, res) {
+
+            const jobId = req.params.id
+            const job = Job.data.find(x => Number(x.id) === Number(jobId))
+            if (!job)
+                return res.send("Job not foud!")
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+            return res.render(views + "job-edit", { job })
+        },
+        update(req, res) {
+            const jobId = req.params.id
+            const job = Job.data.find(x => Number(x.id) === Number(jobId))
+            if (!job)
+                return res.send("Job not foud!")
+
+            const updatedJob = {
+                ...job,
+                name: req.body.name,
+                "total-hours": req.body["total-hours"],
+                "daily-hours": req.body['daily-hours'],
+
+            }
+
+            Job.data = Job.data.map(job => {
+                if (Number(job.id) === Number(jobId)) {
+                    console.log("updatedJob", updatedJob)
+                    job = updatedJob
+                    console.log("job", job)
+                }
+                return job
+            })
+
+            console.log("Job.data", Job.data)
+
+            return res.redirect("/job/" + jobId)
+        },
+        delete(req, res) {
+            const jobId = req.params.id
+
+            Job.data = Job.data.filter(x => Number(x.id) !== Number(jobId))
+
+            return res.redirect("/")
+        }
+    },
+    services: {
+        remainingDays(job) {
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+            const createdDate = new Date(job.created_at)
+            const dueDay = createdDate.getDate() + Number(remainingDays)
+            const dueDateInMs = createdDate.setDate(dueDay)
+
+            const timeDiffInMs = dueDateInMs - Date.now()
+            // transformar em dias
+            const dayInMs = 1000 * 60 * 60 * 24
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+            return dayDiff
+        },
+        calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+    }
+}
+
+
+
+
+
 // req, res
-routes.get('/', (req, res) => res.render(views + "index"))
-routes.get('/job', (req, res) => res.render(views + "job"))
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+routes.get('/', Job.controllers.index)
+routes.get('/job', Job.controllers.create)
+routes.post('/job', Job.controllers.save)
+routes.get('/job/:id', Job.controllers.show)
+routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
+routes.get('/profile', Profile.controllers.index)
+routes.post('/profile', Profile.controllers.update)
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ server.set('view engine', 'ejs');
 // habilitar arquivos estaticos
 server.use(express.static('public'))
 
+// usar req body
+server.use(express.urlencoded({ extended: true }));
+
 // routes
 server.use(routes)
 server.listen(3000, () => console.log('rodando'))
